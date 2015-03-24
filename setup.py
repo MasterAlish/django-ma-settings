9569584594ad43998e1355e12558cf9a68a38003
alisher <masteraalish@gmail.com>
2015-03-24 13:57:45 +0600

    Fix display name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ma-settings',
-    version='1.0.5',
+    version='1.0.6',
     packages=['ma_settings'],
     include_package_data=True,
     license='BSD License',  # example license
